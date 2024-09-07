c31a8e4406b20fa2e14625911b8615ba2be72d97
dapt <cs04153@uoi.gr>
2024-09-07 13:34:40 +0300

    added density calculation

--- Spatial_Grid.py
+++ Spatial_Grid.py
@@ -14,9 +14,11 @@ def apply_gravity(fp):
     return fp
 
 
+
+
 @ti.data_oriented
 class SpatialGrid:
-
+    #TODO: make gridsize self assigned based on computational domain size
     def __init__(self, grid_size: ti.i32):
         self.grid_size = grid_size
         self.cell_size = vec3(x_max / grid_size, y_max / grid_size, z_max / grid_size)
@@ -48,7 +50,7 @@ class SpatialGrid:
         assert PARTICLE_RADIUS * 2 < self.cell_size[1]  # cell must be able to contain at least one particle
         assert PARTICLE_RADIUS * 2 < self.cell_size[2]  # cell must be able to contain at least one particle
 
-
+    #defines the cell id of a particle
     @ti.func
     def get_cell_id(self, pos: ti.template()):
         cell_id = ivec3(0.0, 0.0, 0.0)
@@ -89,13 +91,13 @@ class SpatialGrid:
         for i in range(1, self.grid_size): #cells[i,0,0] init porefix sum sequentially
             self.prefix_sum[i,0,0] = self.prefix_sum[i-1, 0, 0] + self.layer_sum[i-1]
 
-        ti.loop_config(block_dim = self.grid_size)
+        #ti.loop_config(block_dim = self.grid_size)
         for i in range(self.grid_size):
             for j in range(1, self.grid_size): #cells[i,j,0] sequentially accumulate ro_count to prefix
                 self.prefix_sum[i,j,0] = self.prefix_sum[i, j-1, 0] + self.row_sum[i,j-1]
 
 
-        ti.loop_config(block_dim = self.grid_size)
+        #ti.loop_config(block_dim = self.grid_size)
         #ti.loop_config(serialize=True)
         for i in range(self.grid_size):
             for j in range(self.grid_size):
@@ -156,23 +158,73 @@ class SpatialGrid:
             cell_id = self.get_cell_id(pfield[i].p)
             #min & max assure boundary conditions
             x_begin = max(cell_id[0]-1, 0)
-            X_end = min(cell_id[0]+2, self.grid_size)
+            x_end = min(cell_id[0]+2, self.grid_size)
 
             y_begin = max(cell_id[1]-1, 0)
-            Y_end = min(cell_id[1]+2, self.grid_size)
+            y_end = min(cell_id[1]+2, self.grid_size)
 
             z_begin = max(cell_id[2]-1, 0)
-            Z_end = min(cell_id[2]+2, self.grid_size)
+            z_end = min(cell_id[2]+2, self.grid_size)
 
-            for cell_i in range(x_begin, X_end):
-                for cell_j in range(y_begin, Y_end):
-                    for cell_k in range(z_begin, Z_end):
+            for cell_i in range(x_begin, x_end):
+                for cell_j in range(y_begin, y_end):
+                    for cell_k in range(z_begin, z_end):
                         linear_idx = cell_i * self.grid_size * self.grid_size + cell_j * self.grid_size + cell_k
                         for p_id in range(self.head_pointer[linear_idx], self.tail_pointer[linear_idx]):
                             j = self.par_id[p_id]
                             if i < j:   #no overlapping iterations
                                 self.resolve_collision(pfield, i, j)
+                                #pass
                         pfield[i].knn += self.par_count[cell_i, cell_j, cell_k]
+
+    @ti.func
+    def smoothing_kernel(self, radius: ti.f32, dist: ti.f32) -> ti.f32:
+        scale = 15/ (2 * math.pi * radius**(5))
+        value = ti.math.max(0.0, radius - dist)
+        value = (value ** 3) * scale
+        #print(vol)
+        return value
+
+    #search for particles based on radius
+    @ti.kernel
+    def calculate_dens(self, pfield: ti.template(), radius: ti.f32):
+        #print("HELLO")
+
+        for i in range(NUM_PARTICLES):
+            cell_id = self.get_cell_id(pfield[i].p)
+            pfield[i].knn = 0
+            pfield[i].dens = 0.0
+            pfield[i] = apply_gravity(pfield[i])
+            cell_radius = ti.math.ceil(radius / self.cell_size[0], dtype = ti.i32)
+
+            x_begin = max(cell_id[0] - cell_radius, 0)
+            x_end = min(cell_id[0] + cell_radius + 1, self.grid_size)
+
+            y_begin = max(cell_id[1] - cell_radius, 0)
+            y_end = min(cell_id[1] + cell_radius + 1, self.grid_size)
+
+            z_begin = max(cell_id[2] - cell_radius, 0)
+            z_end = min(cell_id[2] + cell_radius + 1, self.grid_size)
+
+            for cell_i in range(x_begin, x_end):
+                for cell_j in range(y_begin, y_end):
+                    for cell_k in range(z_begin, z_end):
+                        linear_idx = cell_i * self.grid_size * self.grid_size + cell_j * self.grid_size + cell_k
+                        for p_id in range(self.head_pointer[linear_idx], self.tail_pointer[linear_idx]):
+                            j = self.par_id[p_id]
+                            rel_pos = pfield[j].p - pfield[i].p
+                            dist = ti.sqrt(rel_pos[0]**2 + rel_pos[1]**2 + rel_pos[2]**2)
+                            #print(radius)
+                            if i < j:
+                                self.resolve_collision(pfield, i, j)
+
+                            if dist < radius:
+                                pfield[i].dens += self.smoothing_kernel(radius, dist) * pfield[j].m
+                                #pfield[i].dens_near += self.smoothing_kernel(radius/2, dist) * pfield[j].m
+
+
+
+
 
 
     @ti.kernel
@@ -203,5 +255,10 @@ class SpatialGrid:
 
         return sum / active_cells
 
+    @ti.kernel
+    def average_density(self, pfield: ti.template()) -> ti.f32:
+        sum = 0.0
+        for i in range(NUM_PARTICLES):
+            sum += pfield[i].dens
 
-
+        return sum / float(NUM_PARTICLES)
