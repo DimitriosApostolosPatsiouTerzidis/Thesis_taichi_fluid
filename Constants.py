9dd61d8b10e0c2fe716bb59e1a291b13282875b3
dapt <cs04153@uoi.gr>
2024-09-10 21:54:52 +0300

    .

--- Constants.py
+++ Constants.py
@@ -1,24 +1,48 @@
 import taichi as ti
+import math
 #ti.init(arch=ti.gpu)
 
 vec3 = ti.math.vec3 #initializing taichi vec3 type
 ivec3 = ti.math.ivec3 #initializing taichi vec3 type
 
+PARTICLE_RADIUS = 0.01
+PADDING = PARTICLE_RADIUS/2
 
-density = 1000.0    #to calculate mass
-stifness = 8e3      #constant affecting collision forces
+
+#p_V = (4 / 3)  * math.pi * (PARTICLE_RADIUS ** 3)  #particle volume
+
 gravity = -9.81
-restitution_coef = 0.09 #constant affecting collision forces
-dt = 0.00004
+dt = 0.00006
 substeps = 30
 
-x_max, y_max, z_max = 1.0, 1.0 , 1.0
+#fluid parameters
+density = 1000.0    #to calculate mass
+p_V = 0.8 * (2*PARTICLE_RADIUS) ** 3    #particle volume
+stifness = 8e4      #constant affecting collision forces
+p_stifness = 50000     #constant affecting pressure forces
+restitution_coef = 0.6 #constant affecting collision forces
+dens_cor_coef = 1.1 #density correction coefficient for near boundary particles
+viscosity = 0.01   #viscosity coefficient
+exponent = 7.0
+surface_tension = 0.01
+
+
+
+
+
+
+
+
+
+#computational domain boundaries
+x_max, y_max, z_max = 1.9, 1.9 , 2.6
 
 #particle space dimensions
-x_dim, y_dim, z_dim = 30, 30, 30
+x_dim, y_dim, z_dim = 40, 60, 40
 NUM_PARTICLES = x_dim * y_dim * z_dim
 print(f"Number of particles: {NUM_PARTICLES}")
-GRID_SIZE = 64
+GRID_SIZE = 50
+
 
 '''
 #GRID_SIZE_VEC = ivec3(50, 100, 50)
@@ -29,8 +53,6 @@ y_max = CELL_SIZE * GRID_SIZE_VEC.y
 z_max = CELL_SIZE * GRID_SIZE_VEC.z
 '''
 
-PARTICLE_RADIUS = 0.004
-PADDING = PARTICLE_RADIUS/2
 #max_vel: ti.f32
 #max_vel = 0.0
 
