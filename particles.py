e95571a713769c9e541889ae8f4a33a55dbfc7e1
dapt <cs04153@uoi.gr>
2024-09-10 21:55:39 +0300

    removed computational functions

--- particles.py
+++ particles.py
@@ -1,5 +1,6 @@
 import taichi as ti
 from Constants import *
+from math_base import *
 import math
 
 ti.init(arch=ti.gpu)
@@ -14,7 +15,8 @@ class fluidPar:     #particle struct
     color: vec3     #color value
     m: ti.f32       #mass calculated based on given density
     dens: ti.f32    #density
-    knn: ti.i32     #number of parrticles in neighborhood
+    pressure: ti.f32      #pressure
+    knn: ti.i32     #number of particles in neighborhood
     #rig: ti.i32    #if 1 represents rigid body voxel
 
 
@@ -27,7 +29,10 @@ def init_particles_pos(pfield: ti.template(), start: ti.template(), reset: ti.i3
         pfield[index].p.x = start[0].x + i * ((PARTICLE_RADIUS * 2) + PADDING) + (ti.random() * (PARTICLE_RADIUS / 4))
         pfield[index].p.y = start[0].y + j * ((PARTICLE_RADIUS * 2) + PADDING) + (ti.random() * (PARTICLE_RADIUS / 4))
         pfield[index].p.z = start[0].z + k * ((PARTICLE_RADIUS * 2) + PADDING) + (ti.random() * (PARTICLE_RADIUS / 4))
-        pfield[index].m = (4 / 3) * density * math.pi * PARTICLE_RADIUS ** 3
+        #pfield[index].m = (4 / 3) * density * math.pi * PARTICLE_RADIUS ** 3
+        pfield[index].m = p_V * density
+        pfield[index].dens = density
+
         pfield[index].id = index
         if reset == 1:
             pfield[index].v = vec3(0.0, 0.0, 0.0)
@@ -35,54 +40,28 @@ def init_particles_pos(pfield: ti.template(), start: ti.template(), reset: ti.i3
             pfield[index].f = vec3(0.0, 0.0, 0.0)
 
 
-@ti.func
-def apply_bc(fp):
-    velocity_damping = 0.5
-    #friction = 0.99
-    x = fp.p.x
-    y = fp.p.y
-    z = fp.p.z
 
-    if y - PARTICLE_RADIUS < 0:
-        fp.p.y = PARTICLE_RADIUS
-        fp.v.y *= -velocity_damping
-        #fp.v.xz *= friction
-    elif y + PARTICLE_RADIUS > y_max:
-        fp.p.y = y_max - PARTICLE_RADIUS
-        fp.v.y *= -velocity_damping
-
-    if z - PARTICLE_RADIUS < 0:
-        fp.p.z = PARTICLE_RADIUS
-        fp.v.z *= -velocity_damping
-    elif z + PARTICLE_RADIUS > z_max:
-        fp.p.z = z_max - PARTICLE_RADIUS
-        fp.v.z *= -velocity_damping
-
-    if x - PARTICLE_RADIUS < 0:
-        fp.p.x = PARTICLE_RADIUS
-        fp.v.x *= -velocity_damping
-    elif x + PARTICLE_RADIUS > x_max:
-        fp.p.x = x_max - PARTICLE_RADIUS
-        fp.v.x *= -velocity_damping
-    return fp
 
 
 @ti.func
 def update(fp):
+    alpha = 0.9
     a = fp.f / fp.m # a = F/m
-    fp.v += (fp.a + a) * dt / 2.0 #verlet integration
+    fp.v += ((fp.a + a) * dt / 1.0) #verlet integration
     fp.p += fp.v * dt + 0.5 * a * dt ** 2
     fp.a = a
 
     return fp
 
-
+#TODO: optimize boundary condition to be applied only to particles in cells near boundary
+#Note: Need to apply boundary check here not in SpatialGrid
 @ti.func
 def update_particle(fp):
     #tempfp = apply_gravity(fp)
     #tempfp = grid.collision_detection(pfield)
+    #tempfp = apply_bc(fp)
     tempfp = update(fp)
-    tempfp = apply_bc(tempfp)
+
     return tempfp
 
 @ti.kernel
